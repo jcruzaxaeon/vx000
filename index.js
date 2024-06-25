88a855e9c7925fd5b2a9ab6644cd5f643555eaa2
jcruz <jcruz@axaeon.com>
2024-06-25 13:50:13 -0700

    define, add model; create a user

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import envConfig from './config/env.js';
 import express from 'express';
-import { Sequelize } from 'sequelize';
+import { Sequelize, DataTypes } from 'sequelize';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -25,6 +25,8 @@ const sequelize = new Sequelize(
     },
 );
 
+
+
 // Test DB Connection
 sequelize.authenticate()
     .then(() => {
@@ -33,6 +35,56 @@ sequelize.authenticate()
     .catch(err => {
         console.error('Unable to connect\n\n', err);
     });
+
+
+
+// Define a model
+const User = sequelize.define('User', {
+    // Define attributes
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING(255),
+        allowNull: true
+    }
+}, {
+    // Other model options
+    tableName: 'users', // Explicitly specifying the table name
+    timestamps: false // Disabling automatic creation of `createdAt` and `updatedAt` columns
+});
+
+// Sync the model with the database
+sequelize.sync()
+    .then(() => {
+        console.log('Database & tables created!');
+
+        // Create a new user
+        return User.create({
+            username: 'aaa',
+            password: 'aaa',
+            name: 'aaa'
+        });
+    })
+    .then(user => {
+        console.log('User created:', user.toJSON());
+    })
+    .catch(err => {
+        console.error('Error creating user\n\n', err);
+    });
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
